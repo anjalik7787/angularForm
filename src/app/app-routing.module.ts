d0c929d5967212f8a1015a8cab4d627d1ba433c9
sadhana <msmoni3@gmail.com>
2021-07-05 19:12:14 +0530

    angularForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngFormComponent } from './ang-form/ang-form.component';
+import { ContentComponent } from './content/content.component';
+import { DatalistComponent } from './datalist/datalist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path:"content", component:ContentComponent},
+  {  path:"Datalist", component:DatalistComponent},
+  {  path: "Angular", component:AngFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { ServiceService } from './service.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DatalistComponent } from './datalist/datalist.component';
+import { AngFormComponent } from './ang-form/ang-form.component'
+
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    DatalistComponent,
+    AngFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+    
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
